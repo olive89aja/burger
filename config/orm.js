f84e53650a5133236972272725c95e307846a1df
OLIVIER AUBERGER <oliviera89@gmail.com>
2019-11-01 15:25:49 -0500

    A lot of files updated. Most notably connection.js and

---
+++ config/orm.js
@@ -0,0 +1,31 @@
+// * In the `orm.js` file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
+// * `selectAll()`
+// * `insertOne()`
+// * `updateOne()`
+
+var connection = require("./connection.js");
+
+var orm = {
+    selectColumn: function(columnName,tableName) {
+        const query = 'SELECT ?? FROM ??'
+    connection.query(query, [columnName,tableName], (error, result) => {
+        if (error) throw error;
+        console.log(result);
+    })
+  },
+
+  selectWhere: function(tableName) {
+    var queryString = "SELECT * FROM burgers WHERE id = 1 OR id = 2 OR id=3";
+    console.log(queryString);
+    connection.query(queryString, [tableName], function(err, result) {
+      if (err) throw err;
+      console.log(result);
+      console.log(result[0].burger_name);
+      console.log(result[1].burger_name);
+      console.log(result[2].burger_name);
+    });
+  },
+};
+
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (id,burger_name,devoured) VALUES (1,'Cheeseburger',TRUE);
+INSERT INTO burgers (id,burger_name,devoured) VALUES (2,'Royal Cheese',TRUE);
+INSERT INTO burgers (id,burger_name,devoured) VALUES (3,'Vegetarian Burger',FALSE);
